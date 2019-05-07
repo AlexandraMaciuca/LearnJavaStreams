d24ec58317c814bc473fdeb0e9f34e15391830b1
Alexandra Maciuca <maciuca.alexandra@garmin.com>
2019-05-07 16:54:33 +0300

    final commit

---
+++ src/Message.java
@@ -0,0 +1,41 @@
+public class Message {
+
+    private String sender;
+    private String receiver;
+    private String message;
+
+    public Message(String sender, String receiver, String message) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return sender + " to " + receiver + " : " + message;
+    }
+}
